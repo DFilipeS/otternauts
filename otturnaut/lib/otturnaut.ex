045cde25ddabff47f1c09f5be37e1f346fddb3fd
Daniel Silva <danfilipesilva@gmail.com>
2025-12-27 22:36:54 +0000

    Created new Elixir application for `otturnaut`.

---
+++ otturnaut/lib/otturnaut.ex
@@ -0,0 +1,18 @@
+defmodule Otturnaut do
+  @moduledoc """
+  Documentation for `Otturnaut`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Otturnaut.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ otturnaut/test/otturnaut_test.exs
@@ -0,0 +1,8 @@
+defmodule OtturnautTest do
+  use ExUnit.Case
+  doctest Otturnaut
+
+  test "greets the world" do
+    assert Otturnaut.hello() == :world
+  end
+end
